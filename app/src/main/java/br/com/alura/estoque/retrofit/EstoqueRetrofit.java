f453ba0e0a3447c196daddde9ea0f6a33bc21249
Alex Felipe <alexfelipevieira@gmail.com>
2019-03-07 10:53:14 -0300

    2.3

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -3,6 +3,7 @@ package br.com.alura.estoque.retrofit;
 import br.com.alura.estoque.model.Produto;
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EstoqueRetrofit {
 
@@ -11,6 +12,7 @@ public class EstoqueRetrofit {
     public EstoqueRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.20.249:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
         produtoService = retrofit.create(ProdutoService.class);
     }
