88c8eaa4c6bdd56caa637df4db0ba4dac6eef2ac
Alex Felipe <alexfelipevieira@gmail.com>
2019-03-07 10:28:07 -0300

    2.2

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,12 +1,21 @@
 package br.com.alura.estoque.retrofit;
 
+import br.com.alura.estoque.model.Produto;
+import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Retrofit;
 
 public class EstoqueRetrofit {
+
+    private final ProdutoService produtoService;
 
     public EstoqueRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.20.249:8080/")
                 .build();
+        produtoService = retrofit.create(ProdutoService.class);
+    }
+
+    public ProdutoService getProdutoService() {
+        return produtoService;
     }
 }
