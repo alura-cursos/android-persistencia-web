00f8ee01a32248d54298ca56c89b4360f87b8f24
Alex Felipe <alexfelipevieira@gmail.com>
2019-03-11 10:09:03 -0300

    4.2

--- app/src/main/java/br/com/alura/estoque/retrofit/service/ProdutoService.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/service/ProdutoService.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import br.com.alura.estoque.model.Produto;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ProdutoService {
 
     @GET("produto")
     Call<List<Produto>> buscaTodos();
+
+    @POST("produto")
+    Call<Produto> salva(@Body Produto produto);
 
 }
